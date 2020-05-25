c092c88854ff8ce3983b663b7d0317c121f27096
José L. Domenech <jldm.upm@gmail.com>
2020-05-25 14:44:46 +0200

    Añadir dos 'stores' nuevos: taxonomies y products
    Espero crear una capa de abstracción sobre estos tipos de
    datos. Dejando el store actual 'app-status' para estado y configuración.

---
+++ src/store/taxonomies/state.js
@@ -0,0 +1,20 @@
+export default function () {
+  return {
+    //
+    taxonomies: {
+      lastupdate: 0,
+
+      additive_classes: null,
+      additives: null,
+      allergens: null,
+      brands: null,
+      countries: null,
+      ingredients_analysis: null,
+      ingredients: null,
+      languages: null,
+      nova_groups: null,
+      nutrient_levels: null,
+      states: null
+    }
+  }
+}
