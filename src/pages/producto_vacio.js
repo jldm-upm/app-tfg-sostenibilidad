f4e28c248285b3c892939f94d3e4831124eca327
José L. Domenech <jldm.upm@gmail.com>
2020-06-06 12:23:01 +0200

    Login: estilo de las pestañas

--- src/pages/producto_vacio.js
+++ src/pages/producto_vacio.js
@@ -2,14 +2,15 @@ export function productoVacio () {
   return {
     code: 'xxxxxxxxxxxxxxxx',
     complete: 1,
-    generic_name: '******* ***',
+    generic_name: '####### ###',
     ingredients_analysis_tags: [
       'en:palm-oil-content-unknown',
       'en:non-vegan',
       'en:vegetarian'],
     labels_tags: ['en:gluten-free'],
-    origins: '--- ------- -------',
-    product_name: '***** *********** ******* ** ****** **',
+    origins: '### ####### #',
+    manufacturing_places: '####### ###, #######',
+    product_name: '##### ########### ####### ## ###### ##',
     sustainability: {
       sustainability_level: 2.5
     }
