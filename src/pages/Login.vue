170615a6a24bb98550d6f5211bc43196734b130b
José L. Domenech <jldm.upm@gmail.com>
2020-06-13 10:19:25 +0200

    Cambiar la posición del botón de configuración.
    * src/pages/Login.vue

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,57 +1,59 @@
 <template>
-  <q-page>
-    <q-tabs
-      v-model="tab"
-      class="text-grey"
-      active-color="primary"
-      indicator-color="primary"
-      align="justify"
-      >
-      <q-tab name="login"
-             icon="person"
-             :label="loginTitle"
-             >
-      </q-tab>
-      <q-tab
-        :disable="!!(loggedInUser)"
-        name="new"
-        icon="person_add"
-        :label="this.$t('login.newTitle')"
+  <div class="column">
+    <user-configuration
+      :user="loggedInUser"
+      resource="/user/save">
+    </user-configuration>
+
+    <q-page>
+      <q-tabs
+        v-model="tab"
+        class="text-grey"
+        active-color="primary"
+        indicator-color="primary"
+        align="justify"
         >
-      </q-tab>
-    </q-tabs>
+        <q-tab name="login"
+               icon="person"
+               :label="loginTitle"
+               >
+        </q-tab>
+        <q-tab
+          :disable="!!(loggedInUser)"
+          name="new"
+          icon="person_add"
+          :label="this.$t('login.newTitle')"
+          >
+        </q-tab>
+      </q-tabs>
 
-    <q-tab-panels
-      v-model="tab"
-      animated
-      class="shadow-2 rounded-borders"
-      >
-      <q-tab-panel
-        name="login">
-        <user-tab
-          v-if="loggedInUser"
-          resource="/user/logout"
-          :user="loggedInUser">
-        </user-tab>
-        <login-tab
-          v-else
-          resource="/user/login">
-        </login-tab>
+      <q-tab-panels
+        v-model="tab"
+        animated
+        class="shadow-2 rounded-borders"
+        >
+        <q-tab-panel
+          name="login">
+          <user-tab
+            v-if="loggedInUser"
+            resource="/user/logout"
+            :user="loggedInUser">
+          </user-tab>
+          <login-tab
+            v-else
+            resource="/user/login">
+          </login-tab>
+        </q-tab-panel>
 
-        <user-configuration
-          :user="loggedInUser"
-          resource="/user/save">
-        </user-configuration>
-      </q-tab-panel>
-
-      <q-tab-panel
-        name="new">
-        <login-new
-          resource="/user/new">
-        </login-new>
-      </q-tab-panel>
-    </q-tab-panels>
-  </q-page>
+        <q-tab-panel
+          name="new">
+          <login-new
+            resource="/user/new">
+          </login-new>
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-page>
+  </div>
 </template>
 
 <script>
