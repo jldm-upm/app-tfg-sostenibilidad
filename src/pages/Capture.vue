f1db2195b74392f037f2ce2bfcd13d1ca26ebc18
José L. Domenech <jldm.upm@gmail.com>
2020-06-16 20:10:57 +0200

    No forzar la orientación al iniciar captura (Cordova)
    * src/pages/Capture.vue
    (iniciarEscaneo) Al iniciar el plugin de escaneo de código de barras,
    no forzar la orientación horizontal, si no que dejar que dependa del dispositivo.

--- src/pages/Capture.vue
+++ src/pages/Capture.vue
@@ -99,7 +99,7 @@ export default {
               // prompt: $t('capture.startScan'), // Android
               resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
               // formats: 'QR_CODE,PDF_417', // default: all but PDF_417 and RSS_EXPANDED
-              // orientation: 'landscape', // Android only (portrait|landscape), default unset so it rotates with the device
+              orientation: 'landscape', // Android only (portrait|landscape), default unset so it rotates with the device
               disableAnimations: true, // iOS
               disableSuccessBeep: false // iOS and Android
             })
