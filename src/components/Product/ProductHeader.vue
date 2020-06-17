cbcb907182ab9b80a484dc7738c7e827abfef03b
José L. Domenech <jldm.upm@gmail.com>
2020-06-17 14:24:09 +0200

    Cambiar la representación de los datos básicos de producto
    * src/components/Product/ProductHeader.vue
    Cambiar el orden del nombre generico y el nombre del producto. También
    se cambia el tamaño de fuente y peso

--- src/components/Product/ProductHeader.vue
+++ src/components/Product/ProductHeader.vue
@@ -1,26 +1,34 @@
 <template>
-<q-item-section>
-      <q-item-label lines="1" class="text-primary">
-        <span class="text-weight-medium">{{ producto.code }}<q-tooltip content-class="bg-white text-primary">{{ producto.code }}</q-tooltip></span>
-      </q-item-label>
-      <q-item-label lines="1">
-        <span class="text-grey-8">{{ producto.generic_name }}<q-tooltip content-class="bg-white text-primary">{{ producto.generic_name }}</q-tooltip></span>
-      </q-item-label>
-      <q-item-label caption lines="1">
+  <q-item-section>
+    <q-item-label lines="1" class="text-primary">
+      <span class="text-weight-medium">{{ producto.code }}<q-tooltip content-class="bg-white text-primary">{{ producto.code }}</q-tooltip></span>
+    </q-item-label>
+    <q-item-label lines="1">
+      <span class="text-h6 text-bold">
+        {{ producto.generic_name }}
+        <q-tooltip content-class="bg-white text-primary">{{ producto.generic_name }}</q-tooltip>
+      </span>
+    </q-item-label>
+
+    <q-item-label caption lines="3">
+      <span class="text-grey-8">
         {{ producto.product_name }}
-        <q-tooltip content-class="bg-white text-primary">{{ producto.product_name }}</q-tooltip>
-      </q-item-label>
-      <q-item-label lines="1" class="q-mt-xs text-body2 text-weight-bold text-primary text-uppercase">
-        <span>
-          <q-rating
-            v-model="sustainability_lvl"
-            readonly
-            size="xs"
-            color="secondary"
-            ></q-rating>
-        </span>
-      </q-item-label>
-    </q-item-section>
+      </span>
+      <q-tooltip content-class="bg-white text-primary">{{ producto.product_name }}</q-tooltip>
+    </q-item-label>
+    <q-item-label
+      lines="1"
+      class="q-mt-xs text-body2 text-weight-bold text-primary text-uppercase">
+      <span>
+        <q-rating
+          v-model="sustainability_lvl"
+          readonly
+          size="xs"
+          color="secondary"
+          ></q-rating>
+      </span>
+    </q-item-label>
+  </q-item-section>
 </template>
 
 <script>
@@ -29,7 +37,7 @@ export default {
 
   computed: {
     sustainability_lvl () {
-      let res = 2.5
+      let res = 3
       if (this.producto.sustainability) {
         if (this.producto.sustainability.sustainability_level) {
           res = this.producto.sustainability.sustainability_level
