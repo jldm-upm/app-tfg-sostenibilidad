bf59f2cf40405a9d2751e5db23515adc00648b92
José L. Domenech <jldm.upm@gmail.com>
2020-06-19 23:47:57 +0200

    Añadidas descripciones a tax. de sostenibilidad
    * src/store/taxonomias/tax/sustainability.js

--- src/store/taxonomias/tax/sustainability.js
+++ src/store/taxonomias/tax/sustainability.js
@@ -1,29 +1,29 @@
 export const val =
   {
-    'en:suitable-packaging': {
+    'en:packaging': {
       description: {
-        en: '',
-        es: ''
+        en: 'The packaging uses sustainable materials and are used in apropiate quantity.',
+        es: 'El empaquetado usa materiales sostenibles y en la cantidad adecuada.'
       },
       name: {
-        en: 'Suitable Packaging',
-        es: 'Empaquetado Adecuado'
+        en: 'Packaging',
+        es: 'Empaquetado'
       }
     },
-    'en:suitable-size': {
+    'en:size': {
       description: {
-        en: '',
-        es: ''
+        en: 'Quantity in the package and serving sizes are adecuate for a sustainable consume',
+        es: 'La cantidad en el paquete y las raciones es adecuada para un consumo sostenible'
       },
       name: {
-        en: 'Suitable Size',
-        es: 'Tamaño Adecuado'
+        en: 'Size',
+        es: 'Tamaño'
       }
     },
     'en:palm-oil': {
       description: {
-        en: '',
-        es: ''
+        en: 'Contains palm-oil',
+        es: 'Contiene aceite de palma'
       },
       name: {
         en: 'Palm-Oil',
@@ -32,8 +32,8 @@ export const val =
     },
     'en:manufacturing': {
       description: {
-        en: '',
-        es: ''
+        en: 'Manufacturing of this product is considered sustainable',
+        es: 'La producción de este producto se considera sostenible'
       },
       name: {
         en: 'Manufacturing',
@@ -42,8 +42,8 @@ export const val =
     },
     'en:transport': {
       description: {
-        en: '',
-        es: ''
+        en: 'Origin/Transport of this product is considered sustainable',
+        es: 'El origien/transporte de este producto hace que se considere sostenible'
       },
       name: {
         en: 'Transport',
@@ -52,8 +52,8 @@ export const val =
     },
     'en:storage': {
       description: {
-        en: '',
-        es: ''
+        en: 'Storage of this product at origin/warehouse/retailers/home is considered sustainable',
+        es: 'El almacenamiento de este producto en el origien/almacén/tienda/hogar es sostenible'
       },
       name: {
         en: 'Storage',
