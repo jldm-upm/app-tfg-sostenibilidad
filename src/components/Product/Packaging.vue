cde556e10ddf6647c1d48e3d1d6cff492db7dd3a
José L. Domenech <jldm.upm@gmail.com>
2020-06-19 21:58:19 +0200

    Descripciones (inglés/español) de la taxonomia sostenibilidad
    * src/components/Product/Packaging.vue

--- src/components/Product/Packaging.vue
+++ src/components/Product/Packaging.vue
@@ -1,13 +1,26 @@
 <template>
   <q-btn-group rounded>
-    <q-icon rounded name="widgets" size="sm" />
-    <q-btn rounded :label="quantity">
+    <q-icon
+      dense
+      rounded
+      name="widgets"
+      size="sm" />
+    <q-btn
+      dense
+      rounded
+      :label="quantity">
       <q-tooltip>{{ $t('product.quantity') }}</q-tooltip>
     </q-btn>
-    <q-btn rounded :label="packaging[0]">
+    <q-btn
+      dense
+      rounded
+      :label="packaging[0]">
       <q-tooltip>{{ $t('product.package_shape') }}</q-tooltip>
     </q-btn>
-    <q-btn rounded :label="packaging[1]">
+    <q-btn
+      dense
+      rounded
+      :label="packaging[1]">
       <q-tooltip>{{ $t('product.package_material') }}</q-tooltip>
     </q-btn>
   </q-btn-group>
