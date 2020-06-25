4c480080618d85470c06d84285055926a85d986c
José L. Domenech <jldm.upm@gmail.com>
2020-06-25 23:36:13 +0200

    Completando página de ayuda
    * src-ayuda/ayuda.html
    * src-ayuda/ayuda.org
    * src-ayuda/img/producto_general.png
    * src-ayuda/img/toolbar.png
    * src/pages/Help.vue
    * src/pages/img/producto_general.png
    * src/pages/img/toolbar.png

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -1,11 +1,326 @@
 <template>
   <q-page>
-    help
+
+<div id="outline-container-orgfe47965" class="outline-2">
+<h2 id="orgfe47965"><span class="section-number-2">1</span> Presentación</h2>
+<div class="outline-text-2" id="text-1">
+<p>
+Esta es la aplicación "Info Sostenibilidad 2020."
+</p>
+
+<p>
+La aplicación "Info Sostenibilidad 2020" es una aplicación que
+permite a sus usuarios consultar información relevante sobre la
+sostenibilidad de productos agroalimentarios.
+</p>
+
+<p>
+La información presentada se complementa con la introducida por los
+usuarios a través de la votación positiva o negativa de la
+sostenibilidad relacionada con un producto.
+</p>
+</div>
+
+<div id="outline-container-org67bee01" class="outline-3">
+<h3 id="org67bee01"><span class="section-number-3">1.1</span> ¿Qué es la sostenibilidad?</h3>
+<div class="outline-text-3" id="text-1-1">
+<blockquote>
+<p>
+Desarrollo que satisfaga las necesidades del presente sin comprometer
+la capacidad de las generaciones futuras para satisfacer sus propias necesidades <sup><a id="fnr.1" class="footref" href="#fn.1">1</a></sup>.
+</p>
+</blockquote>
+
+<p>
+Los usuarios pueden valorar de manera subjetiva, con la guía de los
+datos presentados, las características que pueden hacer un producto
+sostenible o no.
+</p>
+
+<p>
+Por ejemplo, se muestra información sobre el tamaño, forma y
+materiales que componen el empaquetado del producto. Los usuarios
+pueden votar positiva o negativamente los aspecto: 'Empaquetado',
+pero también la 'producción' o 'almacenamiento' están
+relacionados con él.
+</p>
+</div>
+</div>
+</div>
+
+<div id="outline-container-org2de7502" class="outline-2">
+<h2 id="org2de7502"><span class="section-number-2">2</span> Uso</h2>
+<div class="outline-text-2" id="text-2">
+<p>
+La aplicación presenta una pantalla principal con una barra con
+botones para acceder a las funcionalidades principales de la
+aplicación en su parte superior y debajo de esta una entrada de
+búsqueda de producto en la que se puede introducir un código de
+barras o escanearlo.
+</p>
+
+<p>
+El resto de la pantalla se utiliza para mostrar las distintas
+funcionalidades de la aplicación.
+</p>
+
+<p>
+El usuario puede utilizar una pantalla táctil/teclado virtual para
+introducir los datos requeridos. La aplicación está diseñada para
+que sea totalmente funcional usando un teclado y un ratón.
+</p>
+</div>
+
+<div id="outline-container-org56dd776" class="outline-3">
+<h3 id="org56dd776"><span class="section-number-3">2.1</span> Funcionalidades</h3>
+<div class="outline-text-3" id="text-2-1">
+
+<div class="figure">
+<p><a href="img/toolbar.png" alt="imagen toolbar" title="Toolbar" align="center"><img src="./img/toolbar.png" alt="imagen toolbar" title="Toolbar" align="center" /></a>
+</p>
+<p><span class="figure-number">Figura 1: </span>Barra de herramientas y búsqueda de producto</p>
+</div>
+</div>
+
+<div id="outline-container-org6d4595e" class="outline-4">
+<h4 id="org6d4595e"><span class="section-number-4">2.1.1</span> Búsqueda de un producto</h4>
+<div class="outline-text-4" id="text-2-1-1">
+<p>
+En la entrada de búsqueda se puede introducir un código de barras,
+una vez que se cuenta con un código introducido se puede pulsar
+sobre el icono de la lupa para realizar la búsqueda o se puede
+pulsar 'INTRO'.
+</p>
+
+<p>
+Tambíen se puede pulsar sobre botón con el icono de una cámara. Se
+abrirá una pantalla que permite la captura de códigos de barras.
+</p>
+
+<p>
+Se debe pulsar un nuevo botón para iniciar una nueva captura.
+</p>
+
+<p>
+Una vez capturado se iniciará la búsqueda de producto, y se
+mostrará la pantalla de información del producto.
+</p>
+</div>
+
+<div id="outline-container-orge8a3d2e" class="outline-5">
+<h5 id="orge8a3d2e"><span class="section-number-5">2.1.1.1</span> Escaneo</h5>
+<div class="outline-text-5" id="text-2-1-1-1">
+</div>
+<ol class="org-ol">
+<li><a id="org1588b73"></a>Pulsar sobre el botón con el icono de la cámara.<br /></li>
+
+<li><a id="org3bda891"></a>Se mostrará la zona de captura del código de barras.<br /></li>
+
+<li><a id="org4f99615"></a>Centrar la zona de captura sobre el código de barras que se desea escanear.<br /></li>
+</ol>
+</div>
+
+<div id="outline-container-orgae0325a" class="outline-5">
+<h5 id="orgae0325a"><span class="section-number-5">2.1.1.2</span> Introducción del código de barras</h5>
+<div class="outline-text-5" id="text-2-1-1-2">
+</div>
+<ol class="org-ol">
+<li><a id="org1f59e7f"></a>Escribir con el teclado el código que se desea buscar.<br /></li>
+
+<li><a id="org52f587d"></a>Pulsar la tecla 'Enter' o pulsar sobre el icono de la lupa.<br /></li>
+</ol>
+</div>
+</div>
+
+<div id="outline-container-orgaf7f779" class="outline-4">
+<h4 id="orgaf7f779"><span class="section-number-4">2.1.2</span> Acceso a la funcionalidad</h4>
+<div class="outline-text-4" id="text-2-1-2">
+<p>
+Se pueden acceder a las distintas partes de la aplicación mediante
+la barra de herramientas que se encuntra en la parte superior del
+programa.
+</p>
+</div>
+
+<div id="outline-container-org212bad2" class="outline-5">
+<h5 id="org212bad2"><span class="section-number-5">2.1.2.1</span> <q-icon name="person" size="md"/> Configuración y Cuenta de Usuario</h5>
+<div class="outline-text-5" id="text-2-1-2-1">
+<p>
+Se pueden definir algunas opciones para ayudar a la o
+</p>
+</div>
+</div>
+
+<div id="outline-container-org7ca7e34" class="outline-5">
+<h5 id="org7ca7e34"><span class="section-number-5">2.1.2.2</span> <q-icon name="emoji_food_beverage" size="md"/> Información de un producto</h5>
+<div class="outline-text-5" id="text-2-1-2-2">
+<p>
+Para obtener información sobre la sostenibilidad de un producto se
+puede escanear un código de barras que se puede encontrar en el
+empaquetado de los productos o introducirlo manualmente en la barra de
+búsqueda.
+</p>
+</div>
+
+<ol class="org-ol">
+<li><a id="org278511a"></a>Información Mostrada<br />
+<div class="outline-text-6" id="text-2-1-2-2-1">
+
+<div class="figure">
+<p><a href="img/producto_general.png" title="Producto" align="center"><img src="./img/producto_general.png" title="Producto" align="center" /></a>
+</p>
+<p><span class="figure-number">Figura 2: </span>Información general del producto</p>
+</div>
+
+<p>
+La pantalla de producto muestra información del producto buscado.
+</p>
+
+<p>
+Un desplegable con las categorías en las que se puede incluir el producto.
+</p>
+
+<p>
+Información general como nombre, código de producto, país de origen.
+</p>
+
+<p>
+Valoración general del producto por los usuarios.
+</p>
+
+<p>
+Valoración del producto por los usuarios cuantificada con las preferencias del usuario.
+</p>
+
+<p>
+En las pestañas se puede acceder a las valoraciones de
+sostenibilidad del producto, información, ingredientes y
+nutrientes.
+</p>
+</div>
+
+<ol class="org-ol">
+<li><a id="org6a4200b"></a>Lugares de adquisición del producto<br />
+<div class="outline-text-7" id="text-2-1-2-2-1-1">
+<p>
+Pulsando sobre el mapa se muestra una lista donde se puede adquirir el producto.
+</p>
+</div>
+</li>
+
+<li><a id="org7569b31"></a>Información del producto<br />
+<div class="outline-text-7" id="text-2-1-2-2-1-2">
+<p>
+Se encuentra en la pestaña 'Información'.
+</p>
+
+<p>
+Se muestra información sobre si el producto esta libre de aceite de
+palma, si es vegetariano o si es vegano mediante un código de
+colores.
+</p>
+<ul class="org-ul">
+<li><span class="underline">Aceite de palma</span>: Indicado mediante la abreviatura <b>PO</b>. Será de
+color rojo si el producto NO contiene aceite de palma, verde si
+contiene o gris si no se tiene esta información.</li>
+<li><span class="underline">Vegetariano</span>: Indicado mendiante la abreviatura <b>Vt</b>. Será de
+color rojo si el producto NO es vegetariano, verde si lo es o
+gris si no se tiene esta información.</li>
+<li><span class="underline">Vegano</span>: Indicado mediante la abreviatura <b>Vg</b>. Será de color
+rojo si el producto NO es vegano, verde si lo es o gris si no se
+tiene esta información.</li>
+</ul>
+
+<p>
+El origen del producto, de donde se importa y donde se produce. Junto al icono de un avión.
+</p>
+
+<p>
+Huella de carbono por 100g, indicada junto al icono de un coche.
+</p>
+
+<p>
+Información sobre el empaquetado: Peso total del producto, forma y material de empaquetado.
+</p>
+
+<p>
+Etiquetas del producto, tales como la "EU Eco-Label".
+</p>
+</div>
+</li>
+
+<li><a id="org297ade7"></a>Valoraciones de Sostenibilidad<br />
+<div class="outline-text-7" id="text-2-1-2-2-1-3">
+<p>
+Se encuentran en la pestaña de 'Sostenibilidad' de la información del producto.
+</p>
+
+<p>
+Muestra las votaciones positivas, neutras y negativas que ha recibido el producto en cada categoria.
+</p>
+</div>
+</li>
+
+<li><a id="org1393d7b"></a>Ingredientes<br />
+<div class="outline-text-7" id="text-2-1-2-2-1-4">
+<p>
+Se encuentran en la pestaña 'Ingredientes' del producto.
+</p>
+
+<p>
+Muestra los alérgenos que contiene el producto.
+</p>
+
+<p>
+Muestra los aditivos que contiene el producto.
+</p>
+
+<p>
+Muestra una tabla de los ingredientes del producto. La tabla muestra información con el nombre y el porcentaje mínimo (%Min) y máximo (%Max) del ingrediente en el producto.
+</p>
+</div>
+</li>
+
+<li><a id="org0ab83e1"></a>Nutrientes<br />
+<div class="outline-text-7" id="text-2-1-2-2-1-5">
+<p>
+Se encuentran en la pestaña 'Nutrientes' del producto.
+</p>
+
+<p>
+Muestra una tabla con los nutrientes que contiene el producto. La tabla muestra información con el nutriente, la cantidad total (valor), las unidades en que se mide (unidad) y la cantidad en 100 gramos (100g).
+</p>
+</div>
+</li>
+</ol>
+</li>
+</ol>
+</div>
+</div>
+</div>
+</div>
+<div id="footnotes">
+<h2 class="footnotes">Nota al pie de p&aacute;gina: </h2>
+<div id="text-footnotes">
+
+<div class="footdef"><sup><a id="fn.1" class="footnum" href="#fnr.1">1</a></sup> <div class="footpara"><p class="footpara">
+De "Our common future" el informe de la Comisión Mundial sobre el Medio Ambiente y el Desarrollo, 1987.
+</p></div></div>
+
+</div>
+</div>
+
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageHelp'
+  name: 'PageHelp',
+
+  data () {
+    return {
+
+    }
+  }
 }
 </script>
