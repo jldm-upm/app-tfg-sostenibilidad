c13d090e0fda8414a007484335f93d629c945d55
José L. Domenech <jldm.upm@gmail.com>
2020-07-02 20:21:35 +0200

    Mostrar imágen del producto.
    Mostrar imágen del producto (una de las opciones posibles en la BD) si
    'useOtherServices' es true, si no mostrar un avatar mock

--- src/components/Product/ProductHeader.vue
+++ src/components/Product/ProductHeader.vue
@@ -1,6 +1,20 @@
 <template>
   <q-list>
     <q-item>
+      <q-item-section avatar>
+        <q-img
+          v-if="useExternalService"
+          spinner-color="white"
+          :src="imagenProducto"
+          style="max-height: 140px; width: 60px"/>
+        <q-avatar
+          v-else
+          size="md"
+          color="primary"
+          text-color="white">
+          P
+        </q-avatar>
+      </q-item-section>
       <q-item-section>
         <q-item-label lines="1">
           <span class="text-weight-regular text-secondary">{{ producto.code }}<q-tooltip content-class="bg-white text-primary">{{ producto.code }}</q-tooltip></span>
@@ -28,11 +42,30 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   name: 'ProductHeader',
 
   components: {
     'product-ratings': require('src/components/Product/ProductRatings.vue').default
+  },
+
+  computed: {
+    imagenProducto () {
+      if (this.useExternalService) {
+        return this.producto.image_url || this.producto.image_thumb_url || this.producto.image_small_url || this.producto.image_front_url || this.producto.image_front_thumb_url || this.producto.image_front_small_url
+      } else {
+        return ''
+      }
+    },
+    useExternalService () {
+      return this.getUseOtherServices()
+    }
+  },
+
+  methods: {
+    ...mapGetters('appStatus', ['getUseOtherServices'])
   },
 
   props: ['producto']
