7b09b68075eb58682060a295fedad0268bc26e5d
José L. Domenech <jldm.upm@gmail.com>
2020-07-05 20:48:30 +0200

    Mostrando mapa
    * src/css/mapbox-gl.0.53.0.css ->
    src/assets/styles/mapbox-gl.0.53.0.css
    
    * src/pages/Map.vue
    ¡Se muestra un mapa!

--- src/pages/Map.vue
+++ src/pages/Map.vue
@@ -1,37 +1,30 @@
 <template>
-  <MglMap></MglMap>
+  <div id='#app'>
+    <mgl-map
+      :accessToken='accessToken'
+      :mapStyle.sync='mapStyle'
+      >
+      <mgl-navigation-control position='top-right'/>
+      <mgl-geolocate-control position='top-right' />
+    </mgl-map>
+  </div>
 </template>
 
 <script>
-import Mapbox from 'mapbox-gl'
-import { MglMap } from 'vue-mapbox'
-
-const ACCESS_TOKEN = ''
-const MAP_STYLE = ''
+import { MglMap, MglNavigationControl, MglGeolocateControl } from 'vue-mapbox'
+import { MAPBOX_TOKEN, MAPBOX_STYLE } from '../statics/PRIVATE.js'
 
 export default {
   components: {
-    MglMap
+    'mgl-map': MglMap,
+    'mgl-navigation-control': MglNavigationControl,
+    'mgl-geolocate-control': MglGeolocateControl
   },
-
   data () {
     return {
-      accessToken: ACCESS_TOKEN, // your access token. Needed if you using Mapbox maps
-      mapStyle: MAP_STYLE // your map style
+      accessToken: MAPBOX_TOKEN,
+      mapStyle: MAPBOX_STYLE
     }
-  },
-
-  created () {
-    // We need to set mapbox-gl library here in order to use it in template
-    this.mapbox = Mapbox
   }
 }
 </script>
-
-<style>
-<style>
-#map {
-    width: 400px;
-    height: 250px;
-}
-</style>
