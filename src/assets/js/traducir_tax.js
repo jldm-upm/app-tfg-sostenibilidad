a3149b34e9d078fbf21624213b90a606cb8fc409
José L. Domenech <jldm.upm@gmail.com>
2020-07-08 16:56:14 +0200

    Mejorar la documentación de traducciones
    * src/assets/js/traducir_tax.js

--- src/assets/js/traducir_tax.js
+++ src/assets/js/traducir_tax.js
@@ -1,3 +1,13 @@
+// Función que devuelve una traducción de un 'símbolo'
+//
+// Utiliza los datos taxonómicos de la BD Open Food Facts
+//
+// Parámetros:
+// - valor: 'símbolo' (como string) a traducir
+// - taxomomiaJSON: objeto (representa JSON) con símbolos y sus datos asociados
+// - lang: lenguaje al que traducir
+// Devuelve:
+// Una cadena de texto con la traducción
 export function traducirTax (valor, taxonomiaJSON, lang) {
   let res = valor
 
