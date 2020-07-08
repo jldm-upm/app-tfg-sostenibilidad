c53871a730df7a0ccae920753bc95e4dbb056562
José L. Domenech <jldm.upm@gmail.com>
2020-07-08 16:30:49 +0200

    Eliminar función no usada
    * src/components/Product/ProductSustainability.vue
    La función no usada es 'actualizarEnServidor' que se encuentra ahora
    en src/components/Product/VotableSus.vue

--- src/components/Product/ProductSustainability.vue
+++ src/components/Product/ProductSustainability.vue
@@ -62,63 +62,6 @@ export default {
       }
     },
 
-    async actualizarEnServidor (codigo, sustainability, valor) {
-      const baseURL = this.getBaseURL()
-      const url = `${baseURL}/user/vote/${codigo}/${sustainability}/${valor}`
-
-      let type = ''
-      let icon = ''
-      let message = ''
-      this.$q.loading.show()
-      let res = await this.$axios.post(url, this.usuario)
-        .then(response => {
-          this.$q.loading.hide()
-          if (response.data.status === 1) {
-            // this.votarSostenibilidad({ code: this.producto.code, sus: this.sus, val: valor })
-            type = 'possitive'
-            icon = 'cloud_done'
-            message = this.$t('off.voted')
-
-            res = response
-          } else {
-            type = 'negative'
-            icon = 'cloud_done'
-            message = `${this.$t('off.errors.voted')} ${response.data.status_verbose}`
-
-            res = null
-          }
-        })
-        .catch(error => {
-          type = 'negative'
-          message = this.$t('off.errors.serverProblem')
-
-          this.setLastError(error)
-
-          if (error.response) {
-            message = `${this.$t('off.errors.serverProblem')} Http.Status: ${error.response.status}`
-          } else if (error.request) {
-            message = `${this.$t('off.errors.serverProblem')} Http: ${JSON.stringify(error)}`
-          } else {
-            message = `${this.$t('off.errors.serverProblem')} ${this.$t('off.errors.notResponse')}`
-          }
-
-          res = null
-        })
-        .then(() => {
-          this.$q.loading.hide()
-
-          this.$q.notify({
-            type: type,
-            icon: icon,
-            message: message
-          })
-
-          return res
-        })
-
-      return res
-    },
-
     votaciones (key) {
       const res = {}
       if (this.producto) {
